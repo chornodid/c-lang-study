ab70e8207640e943578fec4b1f47d1031738d4d9
Oleksandr Chornodid <chornodid@gmail.com>
2026-04-08 00:26:20 -0500

    Finish exercise ch-01/14

--- kernighan-ritchie-exercises/ch-01/14.c
+++ kernighan-ritchie-exercises/ch-01/14.c
@@ -1,12 +1,33 @@
 /*
- * Exercise 1-14: Write a program to print a histogram of the frequencies of different
- * characters in its input.
+ * Exercise 1-14: Write a program to print a histogram of the frequencies of
+ * different characters in its input.
  */
 
 #include <stdio.h>
 
 int main(void) {
-  fprintf(stderr, "PENDING\n");
+  int i, cfhist[256];
+  for (i = 0; i < 256; ++i)
+    cfhist[i] = 0;
+
+  int c;
+  while ((c = getchar()) != EOF)
+    ++cfhist[c];
+
+  int cf, j;
+  for (i = 0; i < 256; ++i) {
+    cf = cfhist[i];
+    if (cf > 0) {
+      if (i >= ' ' && i <= '~')
+        printf("   %c | ", i);
+      else
+        printf("0x%02x | ", i);
+      for (j = 0; j < cf; ++j)
+        printf("█");
+      putchar('\n');
+    }
+  }
+
   return 0;
 }
 
